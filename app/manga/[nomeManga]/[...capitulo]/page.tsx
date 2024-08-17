5a0adf3a6a96873d06be5c71029cd9dfd4863d93
GustavoSilverio <gustavosk290905@gmail.com>
2024-08-16 21:47:53 -0300

    action buttons in cap

--- app/manga/[nomeManga]/[...capitulo]/page.tsx
+++ app/manga/[nomeManga]/[...capitulo]/page.tsx
@@ -1,8 +1,11 @@
 'use client'
 
 import { useObterMangas } from "@/api/controllers/manga";
-import { decodificarString, obterCapituloSelecionado, obterMangaSelecionado } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { codificarString, decodificarString, obterCapituloSelecionado, obterMangaSelecionado } from "@/lib/utils";
+import { Menu, SkipBack, SkipForward } from "lucide-react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function Page(
     { params }: { params: { nomeManga: string, capitulo: string[] } }
@@ -13,8 +16,14 @@ export default function Page(
         isLoading
     } = useObterMangas()
 
+    const router = useRouter()
+
     const mangaSelecionado = obterMangaSelecionado(mangas, decodificarString(params.nomeManga))
-    const capituloSelecionado = obterCapituloSelecionado(mangaSelecionado, decodificarString(params.capitulo[0]))
+    const {
+        capituloAnterior,
+        capituloSelecionado,
+        capituloProximo
+    } = obterCapituloSelecionado(mangaSelecionado, decodificarString(params.capitulo[0]))
 
     return (
         <div className="flex justify-center max-w-[760px] mx-auto mt-4">
@@ -29,7 +38,7 @@ export default function Page(
                             {mangaSelecionado.nome} - {capituloSelecionado?.nomeCapitulo}
                         </h1>
 
-                        <div className="flex flex-col gap-5 p-3 rounded-md bg-slate-800">
+                        <div className="w-full flex flex-col gap-5 p-3 rounded-md bg-slate-800">
                             {capituloSelecionado?.paginas.map((pagina) => {
                                 return (
                                     <Image
@@ -44,7 +53,39 @@ export default function Page(
                             })}
                         </div>
 
+                        <div className="flex w-full gap-4">
+                            <Button
+                                className="w-full flex gap-2"
+                                variant="outline"
+                                onClick={() => router.push(`/manga/${codificarString(mangaSelecionado.nome)}/${codificarString(capituloAnterior as string)}`)}
+                                disabled={!capituloAnterior}
+                            >
+                                <SkipBack className="w-4" />
 
+                                Capítulo anterior
+                            </Button>
+
+                            <Button
+                                className="w-full flex gap-2"
+                                variant="outline"
+                                onClick={() => router.push(`/manga/${codificarString(mangaSelecionado.nome)}`)}
+                            >
+                                <Menu className="w-4" />
+
+                                Todos os capítulos
+                            </Button>
+
+                            <Button
+                                className="w-full flex gap-2"
+                                variant="outline"
+                                onClick={() => router.push(`/manga/${codificarString(mangaSelecionado.nome)}/${codificarString(capituloProximo as string)}`)}
+                                disabled={!capituloProximo}
+                            >
+                                Próximo capítulo
+
+                                <SkipForward className="w-4" />
+                            </Button>
+                        </div>
                     </div>
                 ) : (
                     <h1>Capítulo não encontrado!</h1>
--- lib/utils.ts
+++ lib/utils.ts
@@ -15,9 +15,49 @@ export const decodificarString = (nomeManga: string) => {
 }
 
 export const obterMangaSelecionado = (mangas: Manga[] | undefined, nomeManga: string) => {
-	return mangas?.find(m => m.nome === nomeManga)
+	let mangaSelecionado = mangas?.find(m => m.nome === nomeManga)
+
+	if (mangaSelecionado)
+		mangaSelecionado.capitulos = mangaSelecionado?.capitulos.sort((a, b) => extrairNumeroCapitulo(a.nomeCapitulo) - extrairNumeroCapitulo(b.nomeCapitulo))
+
+
+	return mangaSelecionado
 }
 
 export const obterCapituloSelecionado = (manga: Manga | undefined, capitulo: string) => {
-	return manga?.capitulos.find(c => c.nomeCapitulo === capitulo)
+
+	const indiceAtual = encontrarIndiceCapituloAtual(capitulo, manga?.capitulos.map(c => c.nomeCapitulo) as string[]);
+	const { anterior, proximo } = obterCapitulosAdjacentes(indiceAtual, manga?.capitulos.map(c => c.nomeCapitulo) as string[]);
+
+	return {
+		capituloSelecionado: manga?.capitulos.find(c => c.nomeCapitulo === capitulo),
+		capituloAnterior: anterior,
+		capituloProximo: proximo,
+	}
+}
+
+const extrairNumeroCapitulo = (capitulo: string) => {
+	const regex = /Capítulo (\d+(\.\d+)?)/
+	const match = capitulo.match(regex)
+	return match ? parseFloat(match[1]) : 0
+}
+
+const encontrarIndiceCapituloAtual = (capituloAtual: string, listaCapitulos: string[] | undefined) => {
+	if (!listaCapitulos) return 0
+	return listaCapitulos.findIndex(capitulo => capitulo === capituloAtual);
+}
+
+const obterCapitulosAdjacentes = (indiceAtual: number, listaCapitulos: string[] | undefined) => {
+	let anterior: string | undefined;
+	let proximo: string | undefined;
+
+	if (indiceAtual > 0) {
+		anterior = listaCapitulos?.[indiceAtual - 1];
+	}
+
+	if (indiceAtual < (listaCapitulos ?? [])?.length - 1) {
+		proximo = listaCapitulos?.[indiceAtual + 1];
+	}
+
+	return { anterior, proximo };
 }
