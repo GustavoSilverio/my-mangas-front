2cca5faec47bdd30cfe03cfb455258747729e7c3
GustavoSilverio <gustavosk290905@gmail.com>
2024-10-29 00:38:40 -0300

    add url ro nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -20,6 +20,12 @@ const nextConfig = {
 				port: "",
 				pathname: "/**",
 			},
+			{
+				protocol: 'https',
+				hostname: 'black.slimeread.com',
+				port: '',
+				pathname: '/**',
+			},
 		],
 	},
 }
